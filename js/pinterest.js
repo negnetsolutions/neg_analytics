0b285cc724ab6eb59005b794d397dee43059393c
Andrew Johnson <andy@negnet.co>
2021-11-04 20:50:45 -0500

    Fire analytics async and defer

--- js/pinterest.js
+++ js/pinterest.js
@@ -1,7 +1,6 @@
-window.addEventListener('DOMContentLoaded', function() {
 // Start Pinterest Pixel.
 !function(e){if(!window.pintrk){window.pintrk = function () {
-window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+  window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
   n=window.pintrk;n.queue=[],n.version="3.0";var
   t=document.createElement("script");t.async=!0,t.src=e;var
   r=document.getElementsByTagName("script")[0];
@@ -54,5 +53,4 @@ events.registerHandler(new function ga() {
     }
 
   };
-});
 });
